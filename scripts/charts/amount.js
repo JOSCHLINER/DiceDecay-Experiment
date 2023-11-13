c956929aef4fbee60f281780e4dbd14557f7f5cb
Joschliner <joschliner@users.noreply.github.com>
2023-11-13 14:20:41 +0100

    formatted code

--- scripts/charts/amount.js
+++ scripts/charts/amount.js
@@ -1,4 +1,4 @@
-function define()   {
+function define() {
 
     let ctx = document.getElementById('AMOUNTCHART');
 
@@ -7,27 +7,27 @@ function define()   {
         data: {
             labels: [],
             datasets: [{
-            label: 'times rolled ',
-            data: [],
-            fill: true,
-            backgroundColor: ['rgb(54, 162, 235, 0.7)', 'rgb(255, 99, 132, 0.7)', 'rgb(75, 192, 192, 0.7)', 'rgb(255, 159, 64, 0.7)', 'rgb(153, 102, 255, 0.7)', 'rgb(255, 205, 86, 0.7)'],
-            borderColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)', 'rgb(75, 192, 192)', 'rgb(255, 159, 64)', 'rgb(153, 102, 255)', 'rgb(255, 205, 86)'],
-            borderWidth: 3,
-            borderRadius: 4
+                label: 'times rolled ',
+                data: [],
+                fill: true,
+                backgroundColor: ['rgb(54, 162, 235, 0.7)', 'rgb(255, 99, 132, 0.7)', 'rgb(75, 192, 192, 0.7)', 'rgb(255, 159, 64, 0.7)', 'rgb(153, 102, 255, 0.7)', 'rgb(255, 205, 86, 0.7)'],
+                borderColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)', 'rgb(75, 192, 192)', 'rgb(255, 159, 64)', 'rgb(153, 102, 255)', 'rgb(255, 205, 86)'],
+                borderWidth: 3,
+                borderRadius: 4
             }]
         },
         options: {
             indexAxis: 'x',
             responsive: true,
             scales: {
-            y: {
-                beginAtZero: true,
-            }
+                y: {
+                    beginAtZero: true,
+                }
             },
             plugins: {
-            legend: {
-                display: false
-            }
+                legend: {
+                    display: false
+                }
             }
         }
     });
--- scripts/script.js
+++ scripts/script.js
@@ -1,88 +1,88 @@
 window.onload = (event) => {
 
-/* Chart Definition*/
-const amount_chart = define();
+    /* Chart Definition*/
+    const amount_chart = define();
 
 
-/* Event Listeners*/
-const copybutton = document.querySelector('.copy');
-copybutton.addEventListener("click", function(){ copy(amount_chart) });
-copybutton.addEventListener("click", function(){ toggleAnimation('.clip_svg') });
+    /* Event Listeners*/
+    const copybutton = document.querySelector('.copy');
+    copybutton.addEventListener("click", function () { copy(amount_chart) });
+    copybutton.addEventListener("click", function () { toggleAnimation('.clip_svg') });
 
-const calculate = document.querySelector('#input_calcbutton');
-calculate.addEventListener("click", dicethrow);
+    const calculate = document.querySelector('#input_calcbutton');
+    calculate.addEventListener("click", dicethrow);
 
 
-/* Functions */
-function decayanalysis()  {
-    let time = get_input("tid");
-    let sides = get_input("sides");
-    let amountdices = get_input("dices")
+    /* Functions */
+    function decayanalysis() {
+        let time = get_input("tid");
+        let sides = get_input("sides");
+        let amountdices = get_input("dices")
 
-    const dices = new dice(amountdices, sides, time);
-    let data = dices.getdata();
+        const dices = new dice(amountdices, sides, time);
+        let data = dices.getdata();
 
-    edit_graph(decay_chart, data);
-}
-
-function dicethrow()    {
-    let sides = get_input("sides");
-    let amountdices = get_input("dices");
-
-    const dices = new dice(amountdices, sides);
-    let data = dices.count();
-
-    edit_graph(amount_chart, data);
-}
-
-function resetZoomChart(chart)  {
-    chart.resetZoom();
-}
-
-function copy(graph) {
-    let graphdata = graph.data.datasets[0].data;
-    if (graphdata.length > 10)    {
-        if (confirm('The to be copied data can be very large are you sure you want to continue?') == false)  {
-            return false;
-        }
+        edit_graph(decay_chart, data);
     }
-    let prepared_data = graphdata.join('	');
 
-    let cp_element = document.createElement('input');
-    cp_element.setAttribute('value', prepared_data);
-    document.body.appendChild(cp_element);
+    function dicethrow() {
+        let sides = get_input("sides");
+        let amountdices = get_input("dices");
 
-    cp_element.select();
-    document.execCommand('copy');
+        const dices = new dice(amountdices, sides);
+        let data = dices.count();
 
-    cp_element.parentNode.removeChild(cp_element);
-}
+        edit_graph(amount_chart, data);
+    }
 
-function edit_graph(graph, data, labeltemplate = "")  {
-    let label = Array.from(data, (values, i) => labeltemplate + (i + 1));
+    function resetZoomChart(chart) {
+        chart.resetZoom();
+    }
 
-    graph.data.datasets[0].data = data;
-    graph.data.labels = label;
+    function copy(graph) {
+        let graphdata = graph.data.datasets[0].data;
+        if (graphdata.length > 10) {
+            if (confirm('The to be copied data can be very large are you sure you want to continue?') == false) {
+                return false;
+            }
+        }
+        let prepared_data = graphdata.join('	');
 
-    graph.update();
-}
+        let cp_element = document.createElement('input');
+        cp_element.setAttribute('value', prepared_data);
+        document.body.appendChild(cp_element);
 
-function get_input(id)    {
-    let data = document.getElementById(id).value;
-    return parseInt(data);
-}   
+        cp_element.select();
+        document.execCommand('copy');
 
-// function to toggle the .animation class of a element('s)
-function toggleAnimation(element) {
-    let toggleElements = document.querySelectorAll(element);
-    
-    toggleElements.forEach(element => {
-        element.classList.add('animation');
+        cp_element.parentNode.removeChild(cp_element);
+    }
 
-        element.addEventListener("animationend", () => {
-            element.classList.remove('animation');
+    function edit_graph(graph, data, labeltemplate = "") {
+        let label = Array.from(data, (values, i) => labeltemplate + (i + 1));
+
+        graph.data.datasets[0].data = data;
+        graph.data.labels = label;
+
+        graph.update();
+    }
+
+    function get_input(id) {
+        let data = document.getElementById(id).value;
+        return parseInt(data);
+    }
+
+    // function to toggle the .animation class of a element('s)
+    function toggleAnimation(element) {
+        let toggleElements = document.querySelectorAll(element);
+
+        toggleElements.forEach(element => {
+            element.classList.add('animation');
+
+            element.addEventListener("animationend", () => {
+                element.classList.remove('animation');
+            });
         });
-    });
-}
+    }
 
 };
